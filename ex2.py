1938e9e9feb1a36c58a40987d2aa7f3c8ba210c0
Yanice VAZ <yanice.vaz@isen-ouest.yncrea.fr>
2020-03-25 08:15:24 +0100

    test

--- ex2.py
+++ ex2.py
@@ -16,13 +16,16 @@ class Complex:
         return Complex(self.__reel/complex1.__reel,self.__imaginaire/complex1.__imaginaire)
 
     def __abs__(self):
-        return Complex((self.__reel**2 + self.__imaginaire**2) ** (1/2))
+        return (self.__reel**2 + self.__imaginaire**2) ** (1/2)
 
     def __eq__(self, complex1):
-        return Complex(self.__reel == complex1.__reel) and (self.__imaginaire == complex1.__imaginaire)
+        return (self.__reel == complex1.__reel) and (self.__imaginaire == complex1.__imaginaire)
+
     def __ne__(self, complex1):
         return (self.__reel != complex1.__reel) or (self.__imaginaire != complex1.__imaginaire)
 
+    def __str__(self):
+        return (str(self.__reel)  + " + "+ str(self.__imaginaire)+"i")
 if __name__ == "__main__":
     cplx1 = Complex(1, 1)
     cplx2 = Complex(2, 2)
